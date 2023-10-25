6c0ea28ff09b5209e02fcf61ae8190885fe467f7
Sascha <sascha.rose@gmail.com>
2023-10-25 11:45:40 +0200

    fix promise is always defined

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,8 +3,8 @@ import { describe } from "vitest";
 import App from "./App";
 
 describe("App", () => {
-  it("should contain the text 'Vite + React'", () => {
+  it("should contain the text 'Vite + React'", async () => {
     render(<App />);
-    expect(screen.findByText("Vite + React")).toBeDefined();
+    expect(await screen.findByText("Vite + React")).toBeDefined();
   });
 });
